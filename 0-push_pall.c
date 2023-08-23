282b99987f00c83103a73be15e2dc47eaef6e369
Mqelisi Stanley Nyoni <mqelisinyoni77@gmail.com>
2023-08-23 23:02:58 +0000

    rebuilt the whole program

---
+++ 0-push_pall.c
@@ -0,0 +1,88 @@
+#include "monty.h"
+
+/**
+* pushNode - push node to the stack
+* @stack: head pointer
+* @line_number: line number
+*
+* Return: void
+*/
+
+void pushNode(stack_t **stack, unsigned int line_number)
+{
+	char *value;
+	int num;
+	stack_t *new_node;
+
+	value = strtok(NULL, " \t\n");
+	if (value == NULL || !is_integer(value))
+	{
+		fprintf(stderr, "L%d: usage: push integer\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	num = atoi(value);
+
+	new_node = (stack_t *)malloc(sizeof(stack_t));
+	if (new_node == NULL)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
+
+	new_node->n = num;
+	new_node->prev = NULL;
+	new_node->next = *stack;
+
+	if (*stack)
+		(*stack)->prev = new_node;
+
+	*stack = new_node;
+}
+
+/**
+* pallStack - prints all the values on the stack,
+* starting from the top of the stack
+* @stack: head pointer
+* @line_number: line number
+*
+* Return: void
+*/
+
+void pallStack(stack_t **stack, unsigned int line_number)
+{
+	stack_t *current;
+
+	(void)line_number;
+	current = *stack;
+
+	while (current != NULL)
+	{
+		printf("%d\n", current->n);
+		current = current->next;
+	}
+}
+
+/**
+* is_integer - checks if the string is a number
+* @str: string
+*
+* Return: 1 if number else 0
+*/
+
+int is_integer(const char *str)
+{
+	char *endptr;
+
+	if (str == NULL || *str == '\0' || isspace(*str))
+		return (0);
+
+	strtol(str, &endptr, 10);
+
+	return (*endptr == '\0');
+}
+
+instruction_t opcodes[] = {
+	{"push", pushNode},
+	{"pall", pallStack},
+	{NULL, NULL}
+};
--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include "monty.h"
+
 /**
  * main - Entry point
  * @argv: arguments
@@ -7,16 +8,61 @@
  */
 int main(int argc, char *argv[])
 {
-	int fileReturn;
+	FILE *file;
+	char *opcode;
+	char line[1024];
+	unsigned int line_number = 0;
+	int i;
+	stack_t *stack = NULL;
 
-	if (argc == 2)
+	if (argc != 2)
 	{
-		fileReturn = processfile(argv[1]);
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
 	}
-	else
+	file = fopen(argv[1], "r");
+	check_open_file(file, argv);
+	while (fgets(line, sizeof(line), file) != NULL)
 	{
-		printf("USAGE: monty file");
-		return (EXIT_FAILURE);
+		line_number++;
+		opcode = strtok(line, " \t\n");
+		if (opcode)
+		{
+			i = 0;
+			while (opcodes[i].opcode)
+			{
+				if (strcmp(opcode, opcodes[i].opcode) == 0)
+				{
+					opcodes[i].f(&stack, line_number);
+					break;
+				}
+				i++;
+			}
+			if (!opcodes[i].opcode)
+			{
+				fprintf(stderr, "L%d: unknown instruction %s\n", line_number, opcode);
+				fclose(file);
+				exit(EXIT_FAILURE);
+			}
+		}
 	}
-	return (0)
+	fclose(file);
+	return (0);
+}
+
+/**
+* check_open_file - checks if the file passed, can be opened.
+* @file: file
+* @argv: arguments passed
+*
+* Return: nothing, prints error msg to stderr on failure
+*/
+
+void check_open_file(FILE *file, char **argv)
+{
+	if (file == NULL)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
 }
