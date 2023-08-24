40b704c76912bd8f2d621c995bf024a98e7befd4
Mqelisi Stanley Nyoni <mqelisinyoni77@gmail.com>
2023-08-24 19:13:56 +0000

    fixed handling error, case of mod by zero

--- 9-mod.c
+++ 9-mod.c
@@ -15,7 +15,7 @@ void modStack(stack_t **stack, unsigned int line_number)
 	}
 	if ((*stack)->n == 0)
 	{
-		fprintf(stderr, "L%d: division by\n", line_number);
+		fprintf(stderr, "L%d: division by zero\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 
