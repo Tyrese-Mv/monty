c4113fbeeaa3a50d6f05f0ef258e67cdcf686912
Tyrese-Mv <musawenkosi106@gmail.com>
2024-03-29 13:26:35 +0200

    modified main

--- main.c
+++ main.c
@@ -12,28 +12,13 @@ int main(int argc, char *argv[])
     char line[1024];
     unsigned int num_line = 0;
     int i;
-    stack_t *stack = NULL;
+    stack_t *stack = NULL, mod_stack;
 
 
     file = fopen(argv[1], "r");
-    while (fgets(line, sizeof(line), file) != NULL)
-    {
-        num_line++;
-        opcode = strtok(line, " \t\n");
-        if (opcode != NULL)
-        {
-            i = 0;
-            while (opcodes[i].opcode)
-            {
-                if (strcmp(opcode, opcodes[i].opcode) == 0)
-                {
-                    opcodes[i].f(&stack, num_line);
-                    break;
-                }
-                i++;
-            }
-        }
-    }
+
+    mod_stack = readFile(file, &stack);
+    
     fclose(file);
     return (0);
 }
