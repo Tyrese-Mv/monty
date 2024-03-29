ab924d16f3497745f9e7d383b292c08c1b97f720
Tyrese-Mv <musawenkosi106@gmail.com>
2024-03-29 13:27:50 +0200

    added push node file

---
+++ push_node.c
@@ -0,0 +1,40 @@
+#include "monty.h"
+
+/**
+* pushNode - push node to the stack
+* @stack: head pointer
+* @line_number: line number
+*
+* Return: void
+*/
+
+void pushNode(stack_t **stack, unsigned int line_number)
+{
+    char *opcode;
+    int val;
+    stack_t *new_node;
+
+    opcode = strtok(NULL, " \t\n");
+    if (opcode == NULL)
+    {
+        fprintf(stderr, "L%d: usage: push integer\n", line_number);
+		exit(EXIT_FAILURE);
+    }
+
+    val = atoi(opcode);
+
+    new_node = (stack_t *)malloc(sizeof(stack_t));
+    if (new_node == NULL)
+    {
+        fprintf(stderr, "Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+    }
+    new_node->n = val;
+	new_node->prev = NULL;
+	new_node->next = *stack;
+
+	if (*stack)
+		(*stack)->prev = new_node;
+
+	*stack = new_node;
+}
