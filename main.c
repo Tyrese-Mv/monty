152c837090f26e82be9ebc0205128d44ad265b29
Tyrese-Mv <musawenkosi106@gmail.com>
2024-03-29 15:22:19 +0200

    modified main.c

--- main.c
+++ main.c
@@ -8,21 +8,28 @@
 int main(int argc, char *argv[])
 {
 	FILE *file;
-	stack_t *stack = NULL;
-	
-    if (argc != 2)
+	stack_t *stack = NULL, *temp;
+
+	if (argc != 2)
 	{
 		fprintf(stderr, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
 	file = fopen(argv[1], "r");
-    if (file == NULL)
+	if (file == NULL)
 	{
 		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
 	readFile(file, &stack);
 	fclose(file);
+	while (stack != NULL)
+	{
+		temp = stack;
+		stack = stack->next;
+		free(temp);
+	}
+	stack = NULL;
 	return (0);
 }
 
