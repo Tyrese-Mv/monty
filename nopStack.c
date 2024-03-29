bdee514d1f96064fa3ba1a8252a7267f16595732
Tyrese-Mv <musawenkosi106@gmail.com>
2024-03-29 15:23:07 +0200

    added nopStack

---
+++ nopStack.c
@@ -0,0 +1,11 @@
+#include "monty.h"
+/**
+ * nopStack - doesn't do anything
+ * @stack: the stack to manipulate
+ * @line_number: line number of the opcode
+ */
+void nopStack(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+}
