01926675f8f3fffdf4db086d572e0a017c8be29c
Tyrese-Mv <musawenkosi106@gmail.com>
2024-03-29 14:05:21 +0200

    modified main.c

--- main.c
+++ main.c
@@ -8,17 +8,14 @@
 int main(int argc, char *argv[])
 {
     FILE *file;
-    char *opcode; 
-    char line[1024];
-    unsigned int num_line = 0;
-    int i;
-    stack_t *stack = NULL, mod_stack;
-
+    stack_t *stack = NULL;
+    (void)argc;
 
     file = fopen(argv[1], "r");
 
-    mod_stack = readFile(file, &stack);
+    readFile(file, &stack);
     
     fclose(file);
     return (0);
 }
+extern instruction_t opcodes[];
